42ad7b7f608f3901189fb40032ed592acef0d7e4
rjayangeles14 <rjayangeles14@gmail.com>
2020-09-14 14:10:34 +0800

    Email Function
    Commit Email fucntion php

---
+++ php/contactform.php
@@ -0,0 +1,17 @@
+<?php
+
+	if (isset($_POST['submit'])){
+		$name = $_POST['name'];
+		$emailFrom = $_POST['email'];
+		$subject = $_POST['subject'];
+		$message = $_POST['message'];
+	}
+	
+	$mailTo = "rjayangeles@yahoo.com";
+	$headers ="From: ". $emailFrom;
+	$txt = "You have received an e-mail from ".$name.".\n\n".$message;
+
+
+	mail($mailTo, $subject, $txt, $headers);
+	header("Location: index.html?mailsend");
+?>
